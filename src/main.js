953689e855ef608589bd3d2df03f96bdfb0f147e
孔德锦 <kongdejin@longse.net>
2021-01-25 15:16:28 +0800

    feat:完成了基本功能

--- src/main.js
+++ src/main.js
@@ -2,8 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import './plugins/element.js'
+import axios from 'axios'
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://localhost:7000/'
+// 请求拦截器
+axios.interceptors.request.use(config =>{
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 必须返回config
+  console.log(config);
+  return config
+})
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
+Vue.use(VueQuillEditor)
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,72 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home.vue'
 
+import Login from '@/components/admin/login.vue'
+import Register from '@/components/admin/register.vue'
+import Admin from '@/components/admin/admin.vue'
+import Welcome from '@/components/admin/Welcome.vue'
+import Users from '@/components/admin/users/userslist.vue'
+import UsersInfo from '@/components/admin/users/userinfo.vue'
+import Essaylist from '@/components/admin/essay/essaylist.vue'
+import EditEssay from '@/components/admin/essay/editEssay.vue'
+import ShowEssay from '@/components/admin/essay/showEssay.vue'
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
+    // redirect: '/home',
     name: 'Home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/showEssay',
+    name: 'ShowEssay',
+    // redirect: '/home',
+    component: ShowEssay
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+    
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register,
+  },
+  {
+    path: '/admin',
+    name: 'Admin',
+    component: Admin,
+    redirect:'/welcome',
+    children:[ 
+    { path:'/welcome',component:Welcome },
+    { path:'/userslist',component:Users },
+    { path:'/userInfo',component:UsersInfo },
+    { path:'/essaylist',component:Essaylist },
+    { path:'/editEssay',component:EditEssay }, ]
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+// 路由导航守卫
+router.beforeEach((to,from,next)=>{
+  // 如果用户访问的登录页面，则直接访问
+  if (to.path === '/login') return next()
+  if (to.path === '/register') return next()
+  // 从sessionStorage 获取保存的token值
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next ()
 })
 
 export default router
